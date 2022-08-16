fc39e372b476a409273118fb7c3374aa641c1b03
Rafał Olszewski <olszak94@gmail.com>
2022-08-16 17:39:03 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { saveAs } from "file-saver";
+
+const pdfUrl = "/TestPDFfile.pdf";
+
+const download = () => {
+  fetch(pdfUrl)
+    .then((res) => res.blob())
+    .then((blob) => {
+      console.log(blob);
+      saveAs(blob);
+    });
+};
+</script>
+
+<template>
+  <div>
+    <button @click="download">download pdf</button>
+  </div>
+</template>
